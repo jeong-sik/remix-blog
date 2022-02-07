a3aa45bae903e62492ced89e5abe3715a8bdccfd
jeong-sik <yousleepwhen@gmail.com>
2022-02-08 00:17:36 +0900

    fix: post 추가

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -5,7 +5,7 @@ export default function Index() {
         width="100%"
         height="300"
         scrolling="no"
-        frameborder="no"
+        frameBorder="no"
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1164583294&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
       ></iframe>
