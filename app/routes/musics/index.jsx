7283a7bba51abcc455097550d66d97fd4d583694
jeong-sik <yousleepwhen@gmail.com>
2022-06-14 13:40:47 +0200

    update

--- app/routes/musics/index.jsx
+++ app/routes/musics/index.jsx
@@ -14,6 +14,9 @@ export default function Posts() {
       <h2 className="text-white text-[22px] font-extrabold mb-2">공연</h2>
       <ul className="break-keep-all">
         <li className="mb-2">
+          2022-05-01 앨리스피어스 w/탈, 공하임, 셀린셀리셀리느, 지나가던조씨 at 빵
+        </li>
+        <li>
           2022-03-19 앨리스피어스 w/잔치잔치, 스모킹구스, 펑크온파이어 at 빵
         </li>
         <li>
